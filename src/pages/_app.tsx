cb7d57c506a009bde01ba2daf5384b593294604d
markushelminen <markus.helminen@visma.com>
2022-12-11 17:43:30 +0200

    add head to whole app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { type AppType } from "next/app";
-
+import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="">
+      <Head>
+        <title>Coolest Pokèmon by markushelminen</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>    
+  );
 };
 
 export default trpc.withTRPC(MyApp);
